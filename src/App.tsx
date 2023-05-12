dcb79a89aa0c8f189ed8c1b7250ee25187374b80
kirin <kirin.chen1001@gmail.com>
2023-05-12 20:33:03 +0800

    Test env load

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn {process.env.REACT_APP_NOT_SECRET_CODE} React
         </a>
       </header>
     </div>
